286464436dfa1105ceab10b57973fc4bc989aa7a
Kadavar <kadavar32@gmail.com>
2018-02-19 11:26:59 +0200

    update error messages

--- app/services/error_messages.rb
+++ app/services/error_messages.rb
@@ -1,6 +1,6 @@
 module ErrorMessages
   LOW_BALANCE_ERROR = 'Low balance. Please retry later'
   INVALID_AMOUNT_ERROR = 'Invalid amount, must be an Integer and > 0'
-  MAX_ATTEMPTS_ERROR = 'Max attempts error'
+  MAX_ATTEMPTS_ERROR = 'Internal Server Error. Please retry'
   PROCEED_ERROR = 'Inaccessible amount. Available amount'
 end
